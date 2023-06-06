4570e7d6bacbf30458c2c8ca985f55df2478c5ea
mdmubin <mdmubin.dev@gmail.com>
2023-06-06 18:50:57 +0600

    Mapping Profile Configuration

---
+++ Api/Config/MappingProfileConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Api.Entities;
+using Api.Models.Dto;
+using AutoMapper;
+
+namespace Api.Config;
+
+public class MappingProfileConfiguration : Profile
+{
+    public MappingProfileConfiguration()
+    {
+        CreateMap<Item, ItemResultDto>()
+            .ForMember(res => res.SellerId, opt => opt.MapFrom(src => src.UserId));
+
+        // CreateMap<ItemCreationDto, Item>()
+        //     .ForMember(dest => dest.Id, opt => opt.MapFrom(src => Guid.NewGuid()))
+        //     .ForMember(dest => dest.Seller, opt => opt.MapFrom<ItemResolver>());
+    }
+}
