1b860e550b848eb0f26d7c98ebe192e908c89432
Mubin <mdmubin.xyz@gmail.com>
2023-07-05 06:24:38 +0600

    added some DTOs

---
+++ Api/Models/Dto/OrderDto.cs
@@ -0,0 +1,40 @@
+﻿namespace Api.Models.Dto;
+
+public class OrderedItemDto
+{
+    public Guid ItemId { get; set; }
+    public int AmountOrdered { get; set; }
+}
+
+public class OrderResultDto
+{
+    public Guid Id { get; set; }
+
+    public DateTime DateTimeOrdered { get; set; }
+
+    public bool Delivered { get; set; }
+
+    public Guid UserId { get; set; }
+}
+
+public class OrderResultDetailsDto
+{
+    public Guid Id { get; set; }
+
+    public DateTime DateTimeOrdered { get; set; }
+
+    public bool Delivered { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public ICollection<OrderedItemDto> OrderedItems { get; set; }
+}
+
+public class OrderRequestDto
+{
+    public DateTime DateTimeOrdered { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public ICollection<OrderedItemDto> OrderedItems { get; set; }
+}
---
+++ Api/Models/Dto/ReviewDto.cs
@@ -0,0 +1,27 @@
+﻿namespace Api.Models.Dto;
+
+public class ReviewRequestDto
+{
+    public int Rating { get; set; }
+
+    public string Content { get; set; } = string.Empty;
+
+    public Guid ItemId { get; set; }
+
+    public Guid UserId { get; set; }
+}
+
+public class ReviewResultDto
+{
+    public Guid Id { get; set; }
+
+    public int Rating { get; set; }
+
+    public string Content { get; set; } = string.Empty;
+
+    public DateTime DateTimeReviewed { get; set; }
+
+    public Guid ItemId { get; set; }
+
+    public Guid UserId { get; set; }
+}
