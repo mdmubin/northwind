4c03dd33e688dcf12cb1b224c37972744d644d38
Mubin <mdmubin.xyz@gmail.com>
2023-07-07 10:52:46 +0600

    added card item

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,13 @@
 import { CssBaseline } from "@mui/material"
 import Navbar from "./components/Navbar";
+import ItemCard from "./components/ItemCard";
 
 export default function App() {
   return (
     <>
       <CssBaseline>
-        <Navbar/>
+        <Navbar />
+        <ItemCard />
       </CssBaseline>
     </>
   );
